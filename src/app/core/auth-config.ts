5bdb9ebc9366169c6f5bc7ed6891cc5bc0ba44d2
TianZeng <jingtian.zeng@globetools.com>
2021-12-23 14:47:36 +0800

    Add dev6 GUC support.

--- src/app/core/auth-config.ts
+++ src/app/core/auth-config.ts
@@ -1,14 +1,28 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
-  issuer: 'https://demo.identityserver.io',
-  clientId: 'implicit',
-  redirectUri: window.location.origin + '/index.html',
-  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
-  scope: 'openid profile email api',
-  silentRefreshTimeout: 5000, // For faster testing
-  timeoutFactor: 0.25, // For faster testing
-  sessionChecksEnabled: true,
-  showDebugInformation: true, // Also requires enabling "Verbose" level in devtools
-  clearHashAfterLogin: false, // https://github.com/manfredsteyer/angular-oauth2-oidc/issues/457#issuecomment-431807040
+  issuer: 'https://cantondeviis.globetools.systems:448',
+  clientId: 'A3S',
+  redirectUri: window.location.origin + '/signin-oidc',
+  silentRefreshRedirectUri: window.location.origin + '/redirect-silentrenew',
+  postLogoutRedirectUri: window.location.origin + '/signout-oidc',
+  scope: 'A3SApi openid profile DeviceDbApi LicenseServiceApi GimsSignalR IotDDSApi',
+  responseType: 'id_token token',
+  // silentRefreshTimeout: 5000, // For faster testing
+  // timeoutFactor: 0.25, // For faster testing
+  // sessionChecksEnabled: true,
+  // showDebugInformation: true, // Also requires enabling "Verbose" level in devtools
+  // clearHashAfterLogin: false, // https://github.com/manfredsteyer/angular-oauth2-oidc/issues/457#issuecomment-431807040
 };
+// export const authConfig: AuthConfig = {
+//   issuer: 'https://demo.identityserver.io',
+//   clientId: 'implicit',
+//   redirectUri: window.location.origin + '/index.html',
+//   silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+//   scope: 'openid profile email api',
+//   silentRefreshTimeout: 5000, // For faster testing
+//   timeoutFactor: 0.25, // For faster testing
+//   sessionChecksEnabled: true,
+//   showDebugInformation: true, // Also requires enabling "Verbose" level in devtools
+//   clearHashAfterLogin: false, // https://github.com/manfredsteyer/angular-oauth2-oidc/issues/457#issuecomment-431807040
+// };
